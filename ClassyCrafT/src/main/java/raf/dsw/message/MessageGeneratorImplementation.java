bf486d0917046bd81171b8e8e4e467ae5018dc33
IRadoevic <iradojevic1@gmail.com>
2023-11-06 15:07:56 +0100

    dodate moguce greske za JTree

--- ClassyCrafT/src/main/java/raf/dsw/message/MessageGeneratorImplementation.java
+++ ClassyCrafT/src/main/java/raf/dsw/message/MessageGeneratorImplementation.java
@@ -16,6 +16,21 @@ public class MessageGeneratorImplementation extends CSubject implements MessageG
                 message = new Message("Node cannot be deleted", "ERROR", timestamp);
                 break;
             //todo
+            case CANT_ADD_CHILD:
+                message = new Message("Cannot add a child to the selected node", "ERROR", timestamp);
+                break;
+            case NAME_ALREADY_EXISTS:
+                message = new Message("Node with this name already exists", "ERROR", timestamp);
+                break;
+            case NO_NODE_SELECTED_FOR_ADD_CHILD:
+                message = new Message("No node was selected for adding child", "ERROR", timestamp);
+                break;
+            case PP_ACTION_FOR_NOTPP:
+                message = new Message("Project as a child of a project can only be executed if project is selected", "WARNING", timestamp);
+                break;
+            case AUTHOR_ACTION_FOR_NOT_PROJECT:
+                message = new Message("Author can only be set up for a project", "WARNING", timestamp);
+                break;
         }
 
         notifySubscriber(message);
