d4e57c3f03a209bc9ea4f92f4c68be6d25320a68
IRadoevic <iradojevic1@gmail.com>
2023-11-12 23:16:11 +0100

    slike

--- ClassyCrafT/src/main/java/raf/dsw/controller/AboutUs.java
+++ ClassyCrafT/src/main/java/raf/dsw/controller/AboutUs.java
@@ -20,13 +20,21 @@ public class AboutUs extends AbstractClassyAction {
         JPanel panel = new JPanel(new FlowLayout(FlowLayout.CENTER));
         panel.add(new JLabel("Andjela Komnenic RN-13/2022"));
         panel.add(new JLabel("Irina Radojevic RN-27/2022"));
+        ImageIcon originalIcon1 = new ImageIcon(getClass().getResource("/images/irina.jpeg"));
+        Image scaledImage1 = originalIcon1.getImage().getScaledInstance(200, 200, Image.SCALE_DEFAULT);
+        ImageIcon scaledIcon1 = new ImageIcon(scaledImage1);
+        JLabel imageLabel1 = new JLabel(scaledIcon1);
+        ImageIcon originalIcon2 = new ImageIcon(getClass().getResource("/images/andjela.png"));
+        Image scaledImage2 = originalIcon2.getImage().getScaledInstance(200, 200, Image.SCALE_DEFAULT);
+        ImageIcon scaledIcon2 = new ImageIcon(scaledImage2);
+        JLabel imageLabel2 = new JLabel(scaledIcon2);
+        panel.add(imageLabel2);
+        panel.add(imageLabel1);
 
         JDialog dialog = new JDialog();
         dialog.add(panel);
-        dialog.setSize(200, 300);
+        dialog.setSize(300, 500);
         dialog.setLocationRelativeTo(null);
         dialog.setVisible(true);
-
-        // treba dodati jos nase slike...
     }
 }
